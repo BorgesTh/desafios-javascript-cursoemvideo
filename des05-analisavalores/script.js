294dbc1757dd3ae4f8dabcbbba1b94978dc5b47f
Thomás Borges <thomasbrgsva@gmail.com>
2021-08-01 21:02:09 -0300

    Add Analisador e ajusta nome dos arquivos

---
+++ des05-analisavalores/script.js
@@ -0,0 +1,59 @@
+let numbers = []
+let optnumbers = []
+let selnums = document.querySelector('select#selnums')
+let res = []
+let soma = 0
+
+for (let c = 1; c <= 5; c++) {
+    res.push(document.querySelector(`p#p${c}`))
+}
+
+function adicionar() {
+    selnums.textContent = ""
+    let numF = Number(document.querySelector('input#txtnum').value)
+    if (numF < 1 || numF > 100) {
+        alert('[ERRO] Número inválido')
+    } else {
+        optnumbers.push(`<option>Número ${numF} adicionado</option>`)
+        numbers.push(numF)
+    }
+    for (let i in optnumbers) {
+        selnums.innerHTML += optnumbers[i]
+    }
+    document.querySelector('input#txtnum').value = ""
+    limparesultados()
+}
+
+function analisar() {
+    limparesultados()
+    numbers.sort()
+    if (numbers.length == 0) {
+        alert('[ERRO] Falta de dados')
+    } else {
+        res[0].innerHTML += ` <span>${numbers.length}</span>`
+        res[1].innerHTML += ` <span>${numbers[numbers.length - 1]}</span>`
+        res[2].innerHTML += ` <span>${numbers[0]}</span>`
+        for (let i in numbers) {
+            soma += numbers[i]
+        }
+        res[3].innerHTML += ` <span>${soma}</span>`
+        res[4].innerHTML += ` <span>${soma / numbers.length}</span>`
+    }
+}
+
+function limpar() {
+    numbers = []
+    optnumbers = []
+    document.querySelector('input#txtnum').value = ""
+    selnums.textContent = ""
+    limparesultados()
+}
+
+function limparesultados() {
+    soma = 0
+    res[0].innerHTML = "Quantidade de números:"
+    res[1].innerHTML = "Maior valor obtido:"
+    res[2].innerHTML = "Menor valor obtido:"
+    res[3].innerHTML = "Soma de todos os valores:"
+    res[4].innerHTML = "Média entre todos os valores:"
+}
